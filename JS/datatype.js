8bd7b533493e70a3f22b7a6921de29690b62ff3a
AshwinPanwar <ashwinpanwar20614@gmail.com>
2025-07-03 19:20:26 +0000

    datatype

---
+++ JS/datatype.js
@@ -0,0 +1,27 @@
+const accountId = 20614 
+let accountEmail ="ashwin@google.com"
+city = "delhi"
+let accountState 
+
+//accountId = 1  when const is used it can not be change futher
+ 
+ accountEmail = "HELLO@gmail.com" 
+
+// console.log(accountEmail);
+console.table([ accountEmail, accountState, accountId, city ]) 
+ 
+
+/*  
+  number => 2 to power 53
+  string 
+  boollen
+  bigint 
+  null  --- type object
+  undefined --- type undefined 
+  symbol (unique) 
+  obejct 
+*/
+ 
+// to find the type use (typeof)
+
+console.log(typeof null);
